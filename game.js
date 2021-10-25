21e5dca7be3173723dbb17ff105a44e0cabb3d36
Kyler Roloff <wakefollower@gmail.com>
2021-10-25 13:52:16 -0500

    placeholder for future game

--- game.js
+++ game.js
@@ -3,3 +3,7 @@ const bodyLine = "|******************************|" + "\n";
 
 let head = headLine * 4;
 let body = bodyLine * 8;
+
+let gameCanvas = document.querySelector("#gameCanvas");
+
+gameCanvas.innerHTML = "placeholder";
